82037b2b439741134a6398797e915adfbc73ac09
Sofian <alehyerr@gmail.com>
2024-10-19 18:32:02 +0800

    no time to name commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+let landingPage = `<div class="landingPage">
+                <menu>
+                    <li><img src="assets/logoWhite.png" alt="" /></li>
+                    <li class="moveRight"><button>Get Started</button></li>
+                </menu>
+                <!-- Main Part -->
+                <div class="centerMargin mainText">
+                    <h1>Jual & Beli</h1>
+                    A network for those who need it.
+                </div>
+
+                <!-- Details -->
+            </div>
+            <div class="centerMargin detailsPage">
+                <img
+                    class="downArrow centerMargin"
+                    src="assets/down-arrow.png"
+                    alt=""
+                /><br />
+
+                <h1>We connect you!</h1>
+            </div>`;
+
+function startup() {}
