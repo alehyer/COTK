71e5b8f8dbe661635229e88a81eaa9e6cf207060
Sofian <alehyerr@gmail.com>
2024-10-19 18:42:00 +0800

    welcome back

--- src/app.js
+++ src/app.js
@@ -21,4 +21,31 @@ let landingPage = `<div class="landingPage">
                 <h1>We connect you!</h1>
             </div>`;
 
-function startup() {}
+let loginPage = `<div class="loginPage">
+                <menu>
+                    <li><img src="assets/logoWhite.png" alt="" /></li>
+                    <li class="normalMenuPadding">Login</li>
+                    <li class="moveRight"><button>Get Started</button></li>
+                </menu>
+                <div class="loginForms centerMargin">
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Enter your username"
+                        required
+                    />
+                    <label for="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Enter your password"
+                        required
+                    /><br />
+                    <button type="submit">Login</button>
+                </div>
+            </div>`;
+
+            
